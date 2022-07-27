f34082c63b5804f99cfa90270f24a5bf45ac9383
sam-rached <rachedsamir@gmail.com>
2022-07-27 22:05:19 +0200

    init projet

---
+++ js/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  let menuToggle = document.querySelector(".toggle");
+  let menu = document.querySelector(".menu");
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+//TYPED
+let typed = new Typed(".typed", {
+  strings: ["Développeur Web Junior"],
+  typeSpeed: 45,
+  backSpeed: 5,
+  smartBackspace: true, 
+  loop: false,
+});
