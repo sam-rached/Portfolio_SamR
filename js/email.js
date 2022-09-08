2c4b61e3e974b0be4889113b7841c3e34bf73f7a
sam-rached <rachedsamir@gmail.com>
2022-09-08 09:23:28 +0200

    nouvelles modifications

---
+++ js/email.js
@@ -0,0 +1,29 @@
+(function () {
+  emailjs.init('J8NPun-Uin39jE7pH');
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+         event.preventDefault();
+      // these IDs from the previous steps
+      emailjs
+        .sendForm('service_h86mkzt', 'template_2h4wfl4', '#contact-form')
+        .then(
+          function (response) {
+            console.log("RÉUSSITE!", response.status, response.text);
+          },
+          function (error) {
+            console.log("ERREUR...", error);
+          }
+        );
+    });
+
+
+};
+
+function clearForm() {
+ document.getElementById("contact-form").reset();
+}
+
